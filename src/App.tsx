55e55514dd1a468f15feb5b48c5eee8f6043f91b
juanUrdaneta <juankorus@gmail.com>
2024-04-22 14:17:48 -0300

    effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useGSAP } from "@gsap/react";
 import "./App.css";
 import Header from "./sections/Header";
 import Navbar from "./sections/Navbar";
@@ -10,10 +11,19 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
+  useGSAP(() => {
+    gsap.to("#box-effect", {
+      bottom: "100vh",
+      ease: "expo.inOut",
+      duration: 1.5,
+      delay: 0.5,
+    });
+  }, []);
   return (
     <>
       <main className="relative">
         <Navbar />
+        <div id="box-effect" className="fixed bottom-0 w-screen h-screen bg-black-soft z-50"></div>
         <Header />
         <Projects />
         <Techstack />
