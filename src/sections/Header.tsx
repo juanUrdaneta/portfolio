64e562cdb76438a56f61016d1987683ebc3be66f
juanUrdaneta <juankorus@gmail.com>
2024-05-14 09:56:31 -0300

    improved header 2

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,10 +1,11 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import React from "react";
 
 const Header = () => {
   useGSAP(() => {
     // recurrent
-    gsap.to("#name-box", {
+    gsap.to("#header-content", {
       y: "100vh",
       opacity: 0.4,
       scale: 0.9,
@@ -15,38 +16,45 @@ const Header = () => {
         scrub: true,
       },
     });
-    gsap.to("#scroll-down-indicator", {
-      y: "100vh",
-      ease: "linear",
-      scrollTrigger: {
-        trigger: "#header",
-        start: "top top",
-        scrub: true,
-      },
-    });
-    gsap.to("#scroll-down-indicator", {
-      opacity: 1,
-      ease: "sine.out",
-      delay: 3,
-    });
+    // gsap.to("#scroll-down-indicator", {
+    //   y: "100vh",
+    //   ease: "linear",
+    //   scrollTrigger: {
+    //     trigger: "#header",
+    //     start: "top top",
+    //     scrub: true,
+    //   },
+    // });
+    // gsap.to("#scroll-down-indicator", {
+    //   opacity: 1,
+    //   ease: "sine.out",
+    //   delay: 3,
+    // });
   }, []);
 
   return (
     <div
       id="header"
-      className="relative w-screen h-screen  flex flex-col justify-center overflow-hidden z-10  bg-black-soft"
+      className="relative w-screen h-screen  flex flex-col justify-start overflow-hidden z-10  bg-black-soft"
     >
-      <section className=" w-full flex flex-col md:flex-row justify-center md:items-center mb-10 ">
-        <div className=" w-full  " id="name-box">
-          <h1 className=" text-left md:text-center font-archivo font-thin md:text-4xl lg:text-[120px] text-2xl select-none text-white-bone mb-6 ">
+      <div id="header-content" className="w-full h-full flex flex-col">
+        <section className=" w-full flex flex-col md:flex-row justify-center md:items-center ">
+          <div className=" w-full  " id="name-box">
+            {/* <h1 className=" text-left md:text-center font-archivo font-thin md:text-4xl lg:text-[120px] text-2xl select-none text-white-bone mb-6 ">
             Juan Urdaneta
-          </h1>
-          <p className="text-white-bone text-xl md:text-center md:text-4xl font-archivo font-thin select-none ">
-            Software Engineer
-          </p>
-        </div>
-      </section>
-      <div
+          </h1> */}
+            <p className="text-white-bone text-xl md:text-center md:text-4xl font-archivo font-thin select-none mt-56 ">
+              Software Engineer
+            </p>
+          </div>
+        </section>
+        <section id="cta" className="flex flex-grow p-10 gap-5">
+          <ProjectCTA name="Shortbread AI" pic="shortbread.jpeg" />
+          <ProjectCTA name="Climatika" pic="climatika.png" />
+          <ProjectCTA name="Dvinum" pic="dvinum.jpeg" />
+        </section>
+      </div>
+      {/* <div
         id="scroll-down-indicator"
         className="absolute bottom-10 self-center hidden md:flex opacity-0"
       >
@@ -55,9 +63,35 @@ const Header = () => {
           alt="sroll down indicator"
           className="w-[50px] h-auto invert animate-bounce "
         />
-      </div>
+      </div> */}
     </div>
   );
 };
 
 export default Header;
+
+const ProjectCTA = ({ name, pic }: { name: string; pic: string }) => {
+  const [hasMouseEntered, setMouseEntered] = React.useState(false);
+  return (
+    <div
+      onMouseEnter={() => setMouseEntered(true)}
+      onMouseLeave={() => setMouseEntered(false)}
+      className="relative w-full h-full max-w-full rounded-lg  overflow-hidden "
+    >
+      <img
+        src={pic}
+        alt={name}
+        className={`w-full h-full object-cover ${
+          hasMouseEntered ? "grayscale-0 scale-105" : "grayscale "
+        } transition-all duration-300`}
+      />
+      {/* <div
+        className={`absolute ${
+          hasMouseEntered ? "bottom-0" : "-bottom-32"
+        } h-32 w-full flex justify-end items-end transition-all duration-300`}
+      >
+        <p className="font-archivo text-6xl text-black-soft">{name}</p>
+      </div> */}
+    </div>
+  );
+};
